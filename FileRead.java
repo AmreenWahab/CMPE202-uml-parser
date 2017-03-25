39a4b855504f84ebf7d4ac5bd784010282ea9cbb
AmreenWahab <amreenabdulwahab@gmail.com>
2017-03-24 22:36:32 -0700

    Update FileRead.java

--- FileRead.java
+++ FileRead.java
@@ -1,16 +1,52 @@
 import java.io.*;
 
+import japa.parser.JavaParser;
 import japa.parser.ParseException;
+import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.visitor.VoidVisitor;
+import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 public class FileRead
 {
-
+	public static class ClassNamePrinter extends VoidVisitorAdapter<Void>
+	
+	{
+		@Override
+		public void visit(ClassOrInterfaceDeclaration cid, Void arg)
+		{
+			super.visit(cid,arg);
+			System.out.println(cid.getName());
+		}
+		
+		
+	}
+	
 		public static void main(String[] args) throws IOException, ParseException 
 		
 		{
-			File[] javaFile = (new File (args[0])).listFiles();
-			ListClassName list = new ListClassName();
-			list.listClass(javaFile);
+			System.out.println(args[0]);
+	
+			File folder = new File(args[0]);
+			File[] files = folder.listFiles();
+	
+			int nooffiles = files.length;
+			VoidVisitor<?> classNameVisitor = new ClassNamePrinter();
+			CompilationUnit[] cu = new CompilationUnit[nooffiles];
+
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].getName().contains(".java")) {
+					FileInputStream in = new FileInputStream(files[i]);
+					cu[i] = JavaParser.parse(in);
+					in.close();
+					if (cu[i] != null) {
+						 classNameVisitor.visit(cu[i], null);
+								
+						
+					}
+				}
+			}
+	
 		}
 		
 	
