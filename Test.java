81cd9d9f9665b0f077f3c537fa606a5cca299f51
AmreenWahab <amreenabdulwahab@gmail.com>
2017-04-04 12:01:05 -0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import net.sourceforge.plantuml.*;
+
+public class Test {
+
+	public static void main(String args[])
+	{
+		
+		
+	 String source = "@startuml \n" + "Class A" + "\n@enduml";
+	 System.out.println(source);
+		
+		SourceStringReader reader = new SourceStringReader(source);
+	
+
+		FileOutputStream png = null;
+		try {
+			png = new FileOutputStream(new File("E:\\img4.png"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
