fc330843304b1b828b7748c9365408557c940415
AmreenWahab <amreenabdulwahab@gmail.com>
2017-04-09 20:59:41 -0700

    added new feature to file

--- FileRead.java
+++ FileRead.java
@@ -1,4 +1,9 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 import japa.parser.JavaParser;
 import japa.parser.ParseException;
@@ -10,6 +15,12 @@ import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 public class FileRead
 {
+	static Multimap<String, String> interfaceMethodData = ArrayListMultimap.create();
+	static Multimap<String, String> classMethodData = ArrayListMultimap.create();
+	static List<String> classNameData = new ArrayList<>();
+	static List<String> interfaceNameData = new ArrayList<>();
+	static boolean interfaceMethodsPresent = false;
+	
 	public static class ClassNamePrinter extends VoidVisitorAdapter<Void>
 	
 	{
@@ -33,13 +44,73 @@ public class FileRead
 	
 			int nooffiles = files.length;
 			
+			
+			
+			
 		//	VoidVisitor<?> classNameVisitor = new ClassNamePrinter();
 			
 			VoidVisitor<?> classOrInterfaceVisitor = new JavaFileParser.ClassOrInterfaceCollector();	
 			VoidVisitor<?> methodVisitor = new JavaFileParser.MethodCollector();
-						
+			VoidVisitor<?> fieldVisitor = new JavaFileParser.FieldCollector();
+			VoidVisitor<?> constructorVisitor = new JavaFileParser.ConstructorCollector();
+			VoidVisitor<?> innerVariableVisitor = new JavaFileParser.InnerVariableCollector();
+			UMLClassStringMaker umlClassStringMaker = new UMLClassStringMaker();
+			UMLDiagramGenerator umlDiagramGenerator = new UMLDiagramGenerator();
+			
 			CompilationUnit[] cu = new CompilationUnit[nooffiles];
 
+			for (int i = 0; i < files.length; i++) 
+			{
+				if (files[i].getName().contains(".java"))
+				{
+					FileInputStream in = new FileInputStream(files[i]);
+					cu[i] = JavaParser.parse(in);
+					in.close();
+					
+					if (cu[i] != null) 
+					{
+						classOrInterfaceVisitor.visit(cu[i], null);
+						methodVisitor.visit(cu[i], null);
+						if(JavaFileParser.isInterface )
+						{
+							interfaceNameData.add(JavaFileParser.interfaceNameList.get(0));
+							if(!JavaFileParser.methodNameList.isEmpty())
+							{
+								interfaceMethodsPresent=true;
+								for(String methodName: JavaFileParser.methodNameList)
+								{
+									interfaceMethodData.put(JavaFileParser.interfaceNameList.get(0), methodName);
+								
+								}
+							}
+							
+							else
+								interfaceMethodsPresent=false;
+								
+						}
+						
+						else
+						{
+							classNameData.add(JavaFileParser.classNameList.get(0));
+							if(!JavaFileParser.methodNameList.isEmpty())
+							{
+								
+								for(String methodName: JavaFileParser.methodNameList)
+								{
+									classMethodData.put(JavaFileParser.classNameList.get(0), methodName);
+								
+								}
+							}
+							
+						}
+						
+						umlClassStringMaker.clear();
+						
+					}
+					
+				}
+
+			}
 			
 			for (int i = 0; i < files.length; i++) 
 			{
@@ -52,56 +123,21 @@ public class FileRead
 					if (cu[i] != null) 
 					{
 						classOrInterfaceVisitor.visit(cu[i], null);
-						
-						//for()
-						
 						methodVisitor.visit(cu[i], null);
+						fieldVisitor.visit(cu[i],null);
+						constructorVisitor.visit(cu[i],null);
+						innerVariableVisitor.visit(cu[i],null);
+						umlClassStringMaker.setString();
+						umlClassStringMaker.clear();
 						
 					}
 					
-					
 				}
-//				
-//				if(JavaFileParser.isInterface)
-//				{
-//					for(String interfaceName: JavaFileParser.interfaceNameList)
-//						System.out.println("Interface  ----  "+interfaceName);
-//					
-//					System.out.println("Methods in Interface");
-//					
-//					for(int j=0; j<=JavaFileParser.methodNameList.size();j++ )
-//					{
-//						System.out.print(JavaFileParser.methodModifierList.get(i));
-//						System.out.print(" "+JavaFileParser.methodReturnTypeList.get(i));
-//						System.out.print(" "+JavaFileParser.methodNameList.get(i));
-//						System.out.print(" "+JavaFileParser.methodParameterList.get(i));
-//						
-//					}
-//					
-//				}
-//				
-//				else
-//				{
-//					
-//					for(String className: JavaFileParser.classNameList)
-//						System.out.println("Class  ----  "+className);
-//					
-//					System.out.println("Methods in Class");
-//					
-//					for(int j=0; j<=JavaFileParser.methodNameList.size();j++ )
-//					{
-//						System.out.print(JavaFileParser.methodModifierList.get(i));
-//						System.out.print(" "+JavaFileParser.methodReturnTypeList.get(i));
-//						System.out.print(" "+JavaFileParser.methodNameList.get(i));
-//						System.out.print(" "+JavaFileParser.methodParameterList.get(i));
-//						
-//					}
-//					
-//				}
-//				
+
 			}
+			
+			
+			umlDiagramGenerator.classDiagramGenerator(UMLClassStringMaker.umlInputString.toString());
 		
 		}
-		
-	
 }
