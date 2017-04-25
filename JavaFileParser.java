70d5192ff7365c39311fc29223d88f8c91fdd9ce
AmreenWahab <amreenabdulwahab@gmail.com>
2017-04-24 21:50:51 -0700

    added method and field parsers

--- JavaFileParser.java
+++ JavaFileParser.java
@@ -172,3 +172,124 @@ public class JavaFileParser
 			//System.out.print(" "+JavaFileParser.constructorParameterList);
 		}
 	}
+
+	//collect method data
+	public static class MethodCollector extends VoidVisitorAdapter<Void>
+	
+	{
+		@Override
+		public void visit(MethodDeclaration md, Void arg)
+		
+		{
+			super.visit(md,arg);
+			
+			methodModifierList.add(md.getModifiers());
+		//	System.out.print(JavaFileParser.methodModifierList);
+			
+				
+				methodReturnTypeList.add(md.getType().toString());
+		//		System.out.print(" "+JavaFileParser.methodReturnTypeList);
+			
+				methodNameList.add(md.getName());
+		//		System.out.print(" "+JavaFileParser.methodNameList);
+				
+				if(!isInterface && md.getBody()!=null)
+				methodBodyList.add(md.getBody().toString());
+		//		System.out.print(" "+JavaFileParser.methodBodyList);
+			
+					if(md.getParameters()!=null)
+					{
+						methodParameterList.add(md.getParameters().toString());
+						//System.out.print(" "+JavaFileParser.methodParameterList);
+					}
+					else
+						methodParameterList.add("");
+					//System.out.print(" "+JavaFileParser.methodParameterList);
+		}
+		
+		
+	}
+	
+	//get fields data
+	public static class FieldCollector extends VoidVisitorAdapter<Void>
+	{
+		@Override
+		public void visit(FieldDeclaration fd, Void arg)
+		
+		{
+			super.visit(fd,arg);
+			
+			fieldModifierList.add(fd.getModifiers());
+			
+			//System.out.println(fieldModifierList);
+			
+			fieldTypeList.add(fd.getType().toString());
+			//System.out.println(fieldTypeList);
+			
+			fieldNameList.add(fd.getVariables().toString());
+			
+			
+			//if(!fieldNameList.isEmpty())
+			for(String name:fieldNameList)
+			{
+				if(name.contains("="))
+				{
+					
+					String newname=name.substring(0, name.indexOf("="));
+					//System.out.println("inside -----" + fieldNameList);
+					fieldNameList.remove(name);
+					//System.out.println("after removing -----" + fieldNameList);
+					fieldNameList.add(newname);
+					//System.out.println("after adding -----" + fieldNameList);
+					
+				}
+						
+			}  
+			//System.out.println(fieldNameList);
+		}
+	}
+	
+	//gather constructor data
+	
+	public static class ConstructorCollector extends VoidVisitorAdapter<Void>
+	{
+		@Override
+		public void visit(ConstructorDeclaration cd, Void arg)
+		
+		{
+			super.visit(cd,arg);
+			
+			constructorModifierList.add(cd.getModifiers());
+		//	System.out.print(JavaFileParser.constructorModifierList);
+				
+			constructorNameList.add(cd.getName());
+			//System.out.print(" "+JavaFileParser.constructorNameList);
+			
+			if(cd.getParameters()!=null)
+			{
+			constructorParameterList.add(cd.getParameters().toString());
+			//System.out.print(" "+JavaFileParser.constructorParameterList);
+			}
+			
+			//System.out.print(" "+JavaFileParser.constructorParameterList);
+		}
+	}
+	
+	//to check for variable declaration inside methods
+	
+	public static class InnerVariableCollector extends VoidVisitorAdapter<Void>
+	{
+		@Override
+		public void visit(VariableDeclarationExpr vd, Void arg)
+		
+		{
+			super.visit(vd,arg);
+								
+			innerVariableTypeList.add(vd.getType().toString());
+			 
+//			System.out.println("InnerVariable "+innerVariableTypeList);
+			
+						
+		}
+	}
+}
